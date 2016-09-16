8201942b57c05603834c736f0a46f6a34f19b392
Osniel <niellt@hotmail.com>
2016-09-16 13:34:23 +0000

    add novos fundos a tela de login

--- login.php
+++ login.php
@@ -4,12 +4,28 @@
         <meta charset="UTF-8">
         <meta name="author" content="Osniel Lopes Teixeira - TIA 316.1940-1">
         <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+        <script type="text/javascript">
+            var banner= new Array()
+            banner[0]="https://media.giphy.com/media/26h0q9kT1eULfuNWg/giphy.gif"
+            banner[1]="https://media.giphy.com/media/l0K4apzqsMv8K4jSw/giphy.gif"
+            banner[2]="https://media.giphy.com/media/mVQ8oOQcPafL2/giphy.gif"
+            banner[3]="https://media.giphy.com/media/3o7ZeDOrdy24GKFkOY/giphy.gif"
+            banner[4]="https://media.giphy.com/media/GqrLv648FAFkk/giphy.gif"
+            banner[5]="https://media.giphy.com/media/3oAt1M3UrBAsn3WvRK/giphy.gif"
+            banner[6]="https://media.giphy.com/media/26ufmXyNfm329F1hm/giphy.gif"
+            banner[7]="https://media.giphy.com/media/l41YuupcfDs5bSOGI/giphy.gif"
+            var random=Math.floor(8*Math.random());
+            document.write("<style>");
+            document.write("body {");
+            document.write(' background:url("' + banner[random] + '");');
+            document.write(" }");
+            document.write("</style>");
+        </script>
         <style type="text/css">
             *{
                 color: white;
             }
             body{
-                background-image:url("https://media.giphy.com/media/26h0q9kT1eULfuNWg/giphy.gif"); 
                 background-size:cover;
             }
             input{
